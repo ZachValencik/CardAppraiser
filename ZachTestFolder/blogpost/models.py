676af0b1c48cf8bb37d538c1231ecda7cc9e772a
ZachValencik <56452654+ZachValencik@users.noreply.github.com>
2021-01-30 17:38:23 -0600

    Part 7
    User account:: the user could change there update profile info and pictures

--- ZachTestFolder/blogpost/models.py
+++ ZachTestFolder/blogpost/models.py
@@ -6,7 +6,7 @@ from flask_login import UserMixin
 def load_user(user_id):
   return User.query.get(int(user_id))
 
-class User(db.Model,UserMixin):
+class User(db.Model,UserMixin): # this is the user in the db
   
   id = db.Column(db.Integer,primary_key=True) #Each user id is unique with a priamry key
   username = db.Column(db.String(20),unique=True,nullable=False) # each username is unique and cant be nulll
@@ -19,13 +19,13 @@ class User(db.Model,UserMixin):
     return f"User('{self.username},{self.email},{self.image_file}')"
 
 
-class Post(db.Model):
+class Post(db.Model): # how the post a user does is made in the db
   
   id = db.Column(db.Integer,primary_key=True) #Each user id is unique with a priamry key
   title =  db.Column(db.String(100),nullable=False)
   date_posted = db.Column(db.DateTime,nullable=False,default=datetime.utcnow) 
   content = db.Column(db.Text,nullable=False)
-  user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+  user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False) # Connecting user and post
 
   def __repr__(self):
     return f"Post('{self.title},{self.date_posted}')"
--- ZachTestFolder/blogpost/routes.py
+++ ZachTestFolder/blogpost/routes.py
@@ -1,7 +1,9 @@
+import secrets,os
+from PIL import Image # this is so we can resize the images so it doesnt take up a lot of sapce if its from a large image
 from flask import render_template,url_for,flash,redirect,request
 from blogpost import app,db,bcrypt
 from blogpost.models import User, Post
-from blogpost.forms import RegistrationForm,LoginForm
+from blogpost.forms import RegistrationForm,LoginForm,UpdateAccountForm
 from flask_login import login_user,current_user,logout_user,login_required
 
 posts = [{  #Dummy Data for practice
@@ -72,7 +74,48 @@ def logout():
   return redirect(url_for('home'))
 
 
-@app.route('/account')
+
+
+
+
+def save_picture(form_picture):
+  random_hex = secrets.token_hex(8)
+  _,f_ext = os.path.splitext(form_picture.filename)
+  picture_fn = random_hex + f_ext
+  picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn)
+
+
+  # this below code makes it so that the file size is only 125 by 25 pixels
+  output_size = (125,125)
+  i = Image.open(form_picture)
+  i.thumbnail(output_size)
+  i.save(picture_path)
+
+  return picture_fn
+
+
+
+
+
+@app.route('/account',methods=['GET','POST'])
 @login_required # we need to login to use account
 def account():
-  return render_template('account.html',title='account')
+  form = UpdateAccountForm()
+  if form.validate_on_submit():
+      if form.picture.data:
+        picture_file = save_picture(form.picture.data)
+        current_user.image_file = picture_file
+
+      current_user.username = form.username.data
+      current_user.email = form.email.data
+      db.session.commit()
+      flash('Your account has been updated','success')
+      return redirect(url_for('account'))
+  elif request.method == 'GET':
+    form.username.data = current_user.username # populate form data with users info
+    form.email.data = current_user.email
+
+
+
+  image_file = url_for('static',filename='profile_pics/'+current_user.image_file)
+  return render_template('account.html',title='account',image_file=image_file,form=form)
