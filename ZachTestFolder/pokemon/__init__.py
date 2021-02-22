a1ac606abb77be86339be3d696a3288c83b7b38a
ZachValencik <56452654+ZachValencik@users.noreply.github.com>
2021-02-22 14:55:20 -0600

    Switched to mysql
    Look out a lot of stuff so That the register succuessfully logs into Lashes Db server that he gave us

--- ZachTestFolder/pokemon/__init__.py
+++ ZachTestFolder/pokemon/__init__.py
@@ -3,19 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_manager
 from flask_mail import Mail
+from flask_mysqldb import MySQL
+#import yaml
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '10fe67da0b56b5384bd201830c37adf3' # import secrets secrets.token_hex(16)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login' # needed for the user to not see the account page while not logged in
-login_manager.login_message_category = 'info' # makes the flash messages look better
-app.config['MAIL_SERVER']= 'smtp.googlemail.com'
-app.config['MAIL_PORT']= 587
-app.config['MAIL_USE_TLS']= True
-app.config['MAIL_USERNAME']= 'gaunterodimwit@gmail.com'
-app.config['MAIL_PASSWORD'] ='Swift109'
 
-mail= Mail(app)
+app.config['MYSQL_HOST']= '45.55.136.114'
+app.config['MYSQL_USER']= 'cards_S2021'
+app.config['MYSQL_PASSWORD']= 'cardsrOK!'
+app.config['MYSQL_DB']= 'cards_S2021'
+
+bcrypt = Bcrypt(app)
+mysql = MySQL(app)
+
+
 from pokemon import routes
