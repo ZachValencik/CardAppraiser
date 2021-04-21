48417b1b3290716aa57559821758aadbb7e7ea8d
gonzal3zz <58669765+gonzal3zz@users.noreply.github.com>
2021-04-21 13:55:14 -0500

    touch ups

--- MainFolder/pokemon/static/guessPokemon.js
+++ MainFolder/pokemon/static/guessPokemon.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const log = console.log,
     array = ["../static/uploads/pika.png","../static/css/snorlax.png",
-        "../static/css/bulbasaur.png","../static/css/poliwhirl.png"]
+        "../static/css/bulbasaur.png","../static/css/poliwhirl.png",
+        "../static/css/mewtwo.png","../static/css/charmander.png"]
     random = Math.floor(Math.random() * array.length),
     target = document.getElementById("char");
   target.src = `${array[random]}`;
