b8580a8a30e26fa2f5b39503a692a180ab0cf00b
gonzal3zz <58669765+gonzal3zz@users.noreply.github.com>
2021-04-21 14:51:07 -0500

    Revert "Merge branch 'master' of https://github.com/ZachValencik/CardAppraiser"
    This reverts commit a53843f69c28c8cf7bb4f61b24187e6e6b38ad0a, reversing
    changes made to f5f476b7a49ebaa098cb188f38668dc07d98301f.

--- MainFolder/pokemon/static/guessPokemon.js
+++ MainFolder/pokemon/static/guessPokemon.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     array = ["../static/uploads/pika.png","../static/css/snorlax.png",
         "../static/css/bulbasaur.png","../static/css/poliwhirl.png",
         "../static/css/mewtwo.png","../static/css/charmander.png",
-        ,"../static/css/evee.png"]
+        "../static/css/evee.png"]
     random = Math.floor(Math.random() * array.length),
     target = document.getElementById("char");
   target.src = `${array[random]}`;
